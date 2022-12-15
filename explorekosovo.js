e9d27641251fda2a942e6781fdbb872d39fa6f0d
Dea Kacoli <dk61403@ubt-uni.net>
2022-12-15 15:52:14 +0100

    First commit

---
+++ explorekosovo.js
@@ -0,0 +1,18 @@
+let mybutton = document.getElementById("myBtn");
+
+// When the user scrolls down 20px from the top of the document, show the button
+window.onscroll = function() {scrollFunction()};
+
+function scrollFunction() {
+  if (document.body.scrollTop > 20 || document.documentElement.scrollTop > 20) {
+    mybutton.style.display = "block";
+  } else {
+    mybutton.style.display = "none";
+  }
+}
+
+// When the user clicks on the button, scroll to the top of the document
+function bottomFunction() {
+
+  document.documentElement.scrollBottom = 0; // For Chrome, Firefox, IE and Opera
+}
