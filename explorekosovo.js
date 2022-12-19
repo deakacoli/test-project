20f2189f6942257be32289f377478b0c63f20f37
Anda Pilingjurka <ap56170@ubt-uni.net>
2022-12-19 14:44:23 +0100

    Javascript added

--- explorekosovo.js
+++ explorekosovo.js
@@ -1,18 +1,20 @@
-let mybutton = document.getElementById("myBtn");
+var body = document.getElementsByClassName('navbar')[0];
+        //body.style.backgroundColor = 'green'
 
-// When the user scrolls down 20px from the top of the document, show the button
-window.onscroll = function() {scrollFunction()};
 
-function scrollFunction() {
-  if (document.body.scrollTop > 20 || document.documentElement.scrollTop > 20) {
-    mybutton.style.display = "block";
-  } else {
-    mybutton.style.display = "none";
-  }
-}
-
-// When the user clicks on the button, scroll to the top of the document
-function bottomFunction() {
-
-  document.documentElement.scrollBottom = 0; // For Chrome, Firefox, IE and Opera
+  window.onscroll = function (event) {
+    var scroll = window.pageYOffset;
+      if (scroll >= 300 && scroll < 600) {
+        // yellow
+        body.style.backgroundColor = 'yellow';
+    } else if (scroll >= 600 && scroll < 1200) {
+        // blue
+        body.style.backgroundColor = 'blue';
+    } else if (scroll >= 1200 && scroll < 1800) {
+        // orange
+        body.style.backgroundColor = 'orange';
+    } else if (scroll >= 1800 && scroll < 3000) {
+        // red
+        body.style.backgroundColor = 'red';
+    }
 }
