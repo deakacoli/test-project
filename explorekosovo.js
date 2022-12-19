927d39186067d3ee90ae41552689431e3efd5749
Anda Pilingjurka <ap56170@ubt-uni.net>
2022-12-19 15:20:47 +0100

    CSS Fix

--- explorekosovo.js
+++ explorekosovo.js
@@ -1,20 +1,18 @@
-var body = document.getElementsByClassName('navbar')[0];
-        //body.style.backgroundColor = 'green'
+/*var body = document.getElementsByClassName('navbar')[0];
 
 
   window.onscroll = function (event) {
     var scroll = window.pageYOffset;
-      if (scroll >= 300 && scroll < 600) {
-        // yellow
-        body.style.backgroundColor = 'yellow';
-    } else if (scroll >= 600 && scroll < 1200) {
-        // blue
-        body.style.backgroundColor = 'blue';
-    } else if (scroll >= 1200 && scroll < 1800) {
-        // orange
-        body.style.backgroundColor = 'orange';
-    } else if (scroll >= 1800 && scroll < 3000) {
-        // red
-        body.style.backgroundColor = 'red';
-    }
+      if (scroll >= 600) {
+    
+        body.style.backgroundColor = 'white';
+        body.style.color='black';
+
+        document.querySelector('header-text').style.color='red';
+
+    } 
+    
+
+    
 }
+*/
